b92658af39cb0825cfd4e303ad516ee14a8785a6
JoaoPedro-Reis <joao.pedro@processasistemas.com.br>
2024-12-12 22:38:26 -0300

    adiciona classname nas divs

--- src/Funcionarios.tsx
+++ src/Funcionarios.tsx
@@ -16,12 +16,12 @@ export default function Funcionario({
   dependentes,
 }: FuncProps) {
   return (
-    <>
+    <div className="funcionariosGeral">
       <div className="nomefunc">Nome: {nome}</div>
       <div className="setor">Setor: {setor}</div>
       <div className="dataAdmissao">Data: {data}</div>
       <div className="salario">Salario: {salario.toLocaleString()}</div>
       <div className="nomeDependente">Nome do dependente: {dependentes}</div>
-    </>
+    </div>
   );
 }
